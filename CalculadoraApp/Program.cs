1c978dd724b886fe83ce8542db3f642bed138a2e
fox <fox@WINDOS>
2025-06-13 14:32:37 -0500

    Agregar archivos de proyecto.

---
+++ CalculadoraApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using CalculadoraCore;
+using System;
+namespace CalculadoraApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorActual = 0;
+            int opcion ;
+        }
+    }
+
+}
